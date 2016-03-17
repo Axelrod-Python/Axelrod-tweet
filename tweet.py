6e1a4e85856e433d83953b9564e5b227793a481b
Vince Knight <vincent.knight@gmail.com>
2016-03-17 12:18:36 +0000

    Adding other symbols

--- tweet.py
+++ tweet.py
@@ -4,9 +4,13 @@ import tweepy
 import csv
 
 rounds = 10
+symbol_pairs = [["C", "D"]] * 3 + \
+               [["👍 ", "👎 "]] + \
+               [["😇 ", "😡 "]]
 
 # Create and play the match
 players = [s() for s in random.sample(axelrod.strategies, 2)]
+symbols = random.choice(symbol_pairs)
 match = axelrod.Match(players, rounds)
 match.play()
 
@@ -14,7 +18,7 @@ match.play()
 tweet = "{} v {}:".format(players[0],
                           players[1])
 tweet += "\n\n"
-tweet += match.sparklines(c_symbol="C", d_symbol="D")
+tweet += match.sparklines(c_symbol=symbols[0], d_symbol=symbols[1])
 tweet += "\n\n"
 tweet += "axelrod.readthedocs.org"
 
